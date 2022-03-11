5bd0e8c7c325acc85f3707eee18ff25252e376b2
Pavan Nagumalli <v-panag@microsoft.com>
2022-03-11 18:40:06 +0530

    Added ProductSKU model with default price as 0 for any SKU

---
+++ PromoCalculator/Models/ProductSKU.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromoCalculator.Models
+{
+	public class ProductSKU
+	{
+		public ProductSKU(char skuId)
+		{
+			SKUId = skuId;
+		}
+
+		public char SKUId { get; set; }
+		public int Price { get; set; } = 0;
+
+
+	}
+}
