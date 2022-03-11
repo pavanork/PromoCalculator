7c7b3e04b03e14570721bd38de6056ff975ed1f4
Pavan Nagumalli <v-panag@microsoft.com>
2022-03-11 18:45:26 +0530

    updated SKU's with default predefined pricing, which can be improved by configuring from caller as well

--- PromoCalculator/Models/ProductSKU.cs
+++ PromoCalculator/Models/ProductSKU.cs
@@ -14,8 +14,23 @@ namespace PromoCalculator.Models
 		}
 
 		public char SKUId { get; set; }
-		public int Price { get; set; } = 0;
+		public int Price { get => FetchPriceBySKU(SKUId); }
 
-
+		private int FetchPriceBySKU(char skuId)
+		{
+			switch (SKUId)
+			{
+				case 'A':
+					return 50;
+				case 'B':
+					return 30;
+				case 'C':
+					return 20;
+				case 'D':
+					return 15;
+				default:
+					return 0;
+			}
+		}
 	}
 }
